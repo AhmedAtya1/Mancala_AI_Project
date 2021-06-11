d1fa65ff2dd9c3ac34968c80ff923bc3a051430c
JohnEssam235 <56760033+JohnEssam235@users.noreply.github.com>
2021-06-11 17:50:26 +0200

    Update BestMove.py

--- BestMove.py
+++ BestMove.py
@@ -2,3 +2,13 @@
 from State import state
 from Node import  node
 
+def create_children(root):
+
+    if (len(root.get_moves())) > 0:
+        for i in root.get_moves():
+
+            temp = state(root.get_pockets(), i, root.get_turn(), root.get_mode())
+            next_state_array = temp.nextState()
+            new_root=node(next_state_array, temp.newTurn(), root.get_mode())
+            new_root.set_depth(root.get_depth())
+            root.get_children().append(new_root)
