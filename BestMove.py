c407069caa1d01d53cef606d12793dd8f7c4c61c
Momen <momenhesham3@gmail.com>
2021-06-11 18:43:49 +0200

    Complete GET_Best_Value function

--- BestMove.py
+++ BestMove.py
@@ -12,3 +12,47 @@ def create_children(root):
             new_root=node(next_state_array, temp.newTurn(), root.get_mode())
             new_root.set_depth(root.get_depth())
             root.get_children().append(new_root)
+
+
+def GET_Best_Value(root_node,depth,isMaximizingPlayer, alpha, beta,parent):
+
+    if root_node.get_number_of_children()==0 and depth != root_node.get_depth() and (not (root_node.get_finish()=="finish")) :
+
+        create_children(root_node)
+
+    if (depth==root_node.get_depth()) or (root_node.get_finish()=="finish"):
+        return root_node.cal_score(1,15,10,10,parent,"player2")
+
+    if isMaximizingPlayer :
+        bestval = - 5000000
+        for i in root_node.get_children():
+            turn=i.get_turn()
+            if turn=='player2':
+                maxmizing=True
+            else:
+                maxmizing=False
+
+            value = GET_Best_Value(i,depth+1,maxmizing, alpha, beta,root_node.get_pockets())
+            bestval = max(bestval, value)
+            i.set_value(bestval)
+            alpha = max(alpha, bestval)
+            if beta <= alpha:
+                break
+
+        return bestval
+
+    else:
+        bestval =  5000000
+        for i in root_node.get_children():
+            turn = i.get_turn()
+            if turn == 'player2':
+                maxmizing = False
+            else:
+                maxmizing = True
+            value = GET_Best_Value(i, depth + 1, maxmizing, alpha, beta,root_node.get_pockets())
+            bestval = min(bestval, value)
+            i.set_value(bestval)
+            beta = min(beta, bestval)
+            if beta <= alpha:
+                break
+        return bestval
