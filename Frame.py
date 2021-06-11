571ac0374d75464fea8cc3a88fc0e7e121d24795
ahmed Emad <1600137@eng.asu.edu.eg>
2021-06-11 17:38:29 +0200

    add functionality of some buttons

--- Frame.py
+++ Frame.py
@@ -127,3 +127,105 @@ class frame1:
 
         self.mode = ""
         self.master.mainloop()
+
+    def Play(self):
+
+        if self.clicked2.get() == "Computer vs Player":
+
+            self.computer = True
+        else:
+            self.computer = False
+
+        if self.computer:
+            self.player_side = self.clicked3.get()
+
+            if self.player_side == "player1":
+                self.computer_side = "player2"
+            else:
+                self.computer_side = "player1"
+        else:
+            self.computer_side = ""
+
+        self.mode = self.clicked.get()
+
+        self.player = [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0]
+
+        self.root.pack_forget()
+        self.root.destroy()
+        img = ImageTk.PhotoImage(Image.open("Mancala_Image.jpg"))
+        my_label = Label(self.master, image=img)
+        my_label.place(x=0, y=0, relwidth=1, relheight=1)
+        self.ai_play = Button(self.master, text="AI Play", command=self.compuer_play, width=6, height=1,
+                              font="Helvetica 20 ",
+                              bg="white", fg="black")
+        if self.computer:
+            self.ai_play.place(x=40, y=50)
+        self.start = Button(self.master, text="Start", command=self.start_playing, width=5, height=1,
+                            font="Helvetica 25 ",
+                            bg="white", fg="black")
+        self.start.place(x=475, y=300)
+        self.b0 = Button(self.master, text="Move", command=self.move0, width=4, height=1, font="Helvetica 14 ",
+                         bg="orange", fg="black")
+        self.b0.place(x=190, y=605)
+        self.b1 = Button(self.master, text="Move", command=self.move1, width=4, height=1, font="Helvetica 14 ",
+                         bg="orange", fg="black")
+        self.b1.place(x=310, y=605)
+        self.b2 = Button(self.master, text="Move", command=self.move2, width=4, height=1, font="Helvetica 14 ",
+                         bg="orange", fg="black")
+        self.b2.place(x=430, y=605)
+        self.b3 = Button(self.master, text="Move", command=self.move3, width=4, height=1, font="Helvetica 14 ",
+                         bg="orange", fg="black")
+        self.b3.place(x=550, y=605)
+        self.b4 = Button(self.master, text="Move", command=self.move4, width=4, height=1, font="Helvetica 14 ",
+                         bg="orange", fg="black")
+        self.b4.place(x=670, y=605)
+        self.b5 = Button(self.master, text="Move", command=self.move5, width=4, height=1, font="Helvetica 14 ",
+                         bg="orange", fg="black")
+        self.b5.place(x=790, y=605)
+        if not self.computer:
+            self.b12 = Button(self.master, text="Move", command=self.move12, width=4, height=1, font="Helvetica 14 ",
+                              bg="cyan", fg="green")
+            self.b12.place(x=190, y=50)
+            self.b11 = Button(self.master, text="Move", command=self.move11, width=4, height=1, font="Helvetica 14 ",
+                              bg="cyan", fg="green")
+            self.b11.place(x=310, y=50)
+            self.b10 = Button(self.master, text="Move", command=self.move10, width=4, height=1, font="Helvetica 14 ",
+                              bg="cyan", fg="green")
+            self.b10.place(x=430, y=50)
+            self.b9 = Button(self.master, text="Move", command=self.move9, width=4, height=1, font="Helvetica 14 ",
+                             bg="cyan", fg="green")
+            self.b9.place(x=550, y=50)
+            self.b8 = Button(self.master, text="Move", command=self.move8, width=4, height=1, font="Helvetica 14 ",
+                             bg="cyan", fg="green")
+            self.b8.place(x=670, y=50)
+            self.b7 = Button(self.master, text="Move", command=self.move7, width=4, height=1, font="Helvetica 14 ",
+                             bg="cyan", fg="green")
+            self.b7.place(x=790, y=50)
+            self.b7["state"] = "disabled"
+            self.b8["state"] = "disabled"
+            self.b9["state"] = "disabled"
+            self.b10["state"] = "disabled"
+            self.b11["state"] = "disabled"
+            self.b12["state"] = "disabled"
+
+        self.b0["state"] = "disabled"
+        self.b1["state"] = "disabled"
+        self.b2["state"] = "disabled"
+        self.b3["state"] = "disabled"
+        self.b4["state"] = "disabled"
+        self.b5["state"] = "disabled"
+        self.ai_play["state"] = "disabled"
+
+        reset_b = Button(self.master, text=" Reset ", command=self.reset, width=5, height=1, font="Helvetica 16 bold",
+                         bg="grey")
+        reset_b.pack(side=BOTTOM)
+
+        self.master.mainloop()
+
+    def save1(self):
+
+        if (self.clicked2.get() == "Computer vs Player"):
+            self.your_turn.pack()
+            self.starting.pack(pady=5)
+
+        self.Play_Button.pack(pady=10)
